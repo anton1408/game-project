d8d42e43656da9a95c7dbbfd7f0571888228079e
luganskyi.anton <luganskyi.anton@gmail.com>
2024-10-27 17:28:32 +0200

    refactor: Minor code improvements in Environment

--- src/Environment.ts
+++ src/Environment.ts
@@ -11,6 +11,7 @@ import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 export default class Environment {
     scene: Scene
     light: DirectionalLight
+    lightHelper: CameraHelper
 
     constructor(scene: Scene, gui: GUI) {
         this.scene = scene
@@ -22,27 +23,31 @@ export default class Environment {
             lightPosZ: number
         } = {
             lightColor: 0xffffff,
-            lightIntensity: Math.PI,
-            lightPosX: 20,
-            lightPosY: 4,
-            lightPosZ: 5
+            lightIntensity: Math.PI * 1.5,
+            lightPosX: 32,
+            lightPosY: 32,
+            lightPosZ: 18
         }
 
-        this.light = new DirectionalLight(data.lightColor, Math.PI)
-        this.light.position.set(20, 4, 5)
-        this.light.shadow.camera.far = 50
+        this.light = new DirectionalLight(data.lightColor, data.lightIntensity)
+        this.light.position.set(data.lightPosX, data.lightPosY, data.lightPosZ)
+        this.light.shadow.camera.far = 100
         this.light.shadow.camera.left = -15
         this.light.shadow.camera.right = 15
         this.light.shadow.camera.top = 10
         this.light.shadow.camera.bottom = -10
+        this.light.shadow.radius = 2
+        this.light.shadow.blurSamples = 2
+        this.light.shadow.mapSize.width = 512 * 3
+        this.light.shadow.mapSize.height = 512 * 3
         this.light.castShadow = true
         this.scene.add(this.light)
 
-        // helper
+        // lightHelper
         // const lightHelper = new DirectionalLightHelper(this.light)
-        const lightHelper = new CameraHelper(this.light.shadow.camera)
-        lightHelper.visible = true
-        this.scene.add(lightHelper)
+        this.lightHelper = new CameraHelper(this.light.shadow.camera)
+        this.lightHelper.visible = false
+        this.scene.add(this.lightHelper)
 
         this.setupGUI(gui, data)
     }
@@ -57,19 +62,20 @@ export default class Environment {
         }
     ) {
         const lightHelperFolder = gui.addFolder('Directional Light')
-        lightHelperFolder.add(this.light, 'visible')
+        lightHelperFolder.add(this.light, 'visible').name('Light on/off')
+        lightHelperFolder.add(this.lightHelper, 'visible').name('lightHelper on/off')
         lightHelperFolder.addColor(data, 'lightColor').onChange(() => {
             this.light.color.set(data.lightColor)
         })
-        lightHelperFolder.add(this.light, 'intensity', 0, 10, 0.1).name('Intensity');
+        lightHelperFolder.add(this.light, 'intensity', 0, 10, 0.1).name('Intensity')
         lightHelperFolder.add(data, 'lightPosX', -50, 50).name('Position X').onChange(() => {
-            this.light.position.x = data.lightPosX;
+            this.light.position.x = data.lightPosX
         })
         lightHelperFolder.add(data, 'lightPosY', -50, 50).name('Position Y').onChange(() => {
-            this.light.position.y = data.lightPosY;
+            this.light.position.y = data.lightPosY
         })
         lightHelperFolder.add(data, 'lightPosZ', -50, 50).name('Position Z').onChange(() => {
-            this.light.position.z = data.lightPosZ;
+            this.light.position.z = data.lightPosZ
         })
         lightHelperFolder.add(this.light.shadow.camera, 'near', 0.1, 100).name('Shadow Near').onChange(() => {
             this.light.shadow.camera.updateProjectionMatrix()
@@ -89,6 +95,7 @@ export default class Environment {
         lightHelperFolder.add(this.light.shadow.camera, 'bottom', -50, 50).name('Shadow Bottom').onChange(() => {
             this.light.shadow.camera.updateProjectionMatrix()
         })
+        lightHelperFolder.close()
     }
 
     async init() {
